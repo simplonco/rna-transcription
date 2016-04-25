9a2b0d01eda35f8f003cd8bc7499908832867a24
Adam Sakhr <adam.sakhr@gmail.com>
2016-04-25 10:41:30 +0200

    LAST CHECK

--- rna-transcription.js
+++ rna-transcription.js
@@ -1,13 +1,18 @@
-var rnaComplementMapping = { C: 'G', G: 'C', A: 'U', T: 'A' };
+var rnaComplementMapping = {
+    C: 'G',
+    G: 'C',
+    A: 'U',
+    T: 'A'
+};
 
 function rnaComplement(dnaNucleotide) {
-  return rnaComplementMapping[dnaNucleotide];
+    return rnaComplementMapping[dnaNucleotide];
 }
 
 function convertDnaNucleotidesToRna(dnaNucleotides) {
-  return dnaNucleotides.replace(/./g, rnaComplement);
+    return dnaNucleotides.replace(/./g, rnaComplement);
 }
 
 if (module) {
-  module.exports = convertDnaNucleotidesToRna;
+    module.exports = convertDnaNucleotidesToRna;
 }
