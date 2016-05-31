cd7eb6fa86f8c41e02788a3e5e5274dbc77f291b
kata-simplonco <ysraka+1@simplon.co>
2016-05-31 11:17:51 +0200

    Update rna-transcription.js

--- rna-transcription.js
+++ rna-transcription.js
@@ -9,9 +9,18 @@
 var DnaTranscriber = function(){};
 
 DnaTranscriber.prototype.toRna = function(dna) {
-//
-// YOUR CODE GOES HERE
-//
-}
+  for (var i = 0; i < dna.length; i++) {
+    if (dna[i] == "T") {
+      dna[i] = "A";
+    } else if (dna[i] == "A") {
+      dna[i] = "T";
+    } else if (dna[i] == "G") {
+      dna[i] = "C";
+    } else { // dna[i] == "C"
+      dna[i] = "G";
+    }
+  }
+  return dna;
+};
 
 module.exports = DnaTranscriber;
